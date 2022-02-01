8fd19dbb7dff0692da0c47ecf6daf34cda8b0fb9
stepanka.klimesova <klimesovastepanka@seznam.cz>
2022-02-01 15:24:48 +0100

    snek

--- src/main/java/com/example/schoolsystem/HelloController.java
+++ src/main/java/com/example/schoolsystem/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.schoolsystem;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox comboGrades;
+
+    @FXML
+    private TextField tfStudent;
+
+    @FXML
+    private ComboBox comboStudent;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboGrades.getItems().add(1);
+        comboGrades.getItems().add(2);
+        comboGrades.getItems().add(3);
+        comboGrades.getItems().add(4);
+        comboGrades.getItems().add(5);
+
     }
+
+    private void fList(){
+        comboStudent.getItems().add(tfStudent.getText());
+    }
+
 }
